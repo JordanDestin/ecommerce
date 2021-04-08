211d5d60d6f67770275949abceda4da9e38d426e
jdestin <destin.jord@gmail.com>
2021-04-08 22:42:35 +0200

    WIP

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,29 +4,17 @@
 
 <div class="row">
   <div id="carousel" class="carousel slide" data-ride="carousel">
-
-    <div class="carousel-inner col-lg-12">
-      <div class="carousel-item col-lg-12 active">
-        <img class="d-block w-100" src="{{asset('images/thumbs/1617634037.jpg') }}" alt="First slide">
+     <div class="carousel-inner col-lg-12">
+      @foreach($products as $key => $product)
+      <div class="carousel-item {{$key == 0 ? 'active' : '' }}"> 
+        <img class="d-block w-100"src="{{asset('images/thumbs/'.$product->image  ) }}" alt="First slide">
         <div class="carousel-caption">
           <h5>test1</h5>
           <p>test1test1test1</p>
         </div>
       </div>
-      <div class="carousel-item col-lg-12">
-        <img class="d-block w-100" src="{{asset('images/thumbs/1617634070.jpg') }}" alt="Second slide">
-        <div class="carousel-caption">
-          <h5>test2</h5>
-          <p>test2test2test2</p>
-        </div>
-      </div>
-      <div class="carousel-item col-lg-12">
-        <img class="d-block w-100" src="{{asset('images/thumbs/1617634091.jpg') }}" alt="Third slide">
-        <div class="carousel-caption">
-          <h5>test3</h5>
-          <p>test3test3test3</p>
-        </div>
-      </div>
+      @endforeach
+
     </div>
     <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -39,7 +27,57 @@
   </div>
 </div>
 
+<div class="container">
+  <div class="row mt-5">
+    <div class="col-lg-12">
+      <div class="row category1">
+        <div class="col lg-6 sous-category1"></div>
+        <div class="col lg-6 sous-category2"></div>
+      </div>
+      <div class="row category2">
+        <div class="col lg-4 sous-category3"></div>
+        <div class="col lg-4 sous-category4"></div>
+        <div class="col lg-4 sous-category5"></div>
+      </div>
+    </div>
+  </div>
 
+
+  <div class="row mt-5">
+    <div id="carousel-popular" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner row w-100 mx-auto">
+        @foreach($products->chunk(3) as $key => $product)
+        <div class="carousel-item {{$key == 0 ? 'active' : '' }}"> 
+          <div class="row">
+            
+            @foreach ($product as $item )
+            
+            <img class="imgpopular rounded mx-auto d-block" src="{{asset('images/thumbs/'.$item->image  ) }}" alt="{{ $item->title }}">
+            <div class="carousel-caption">
+              <h5 class="">{{ $item->title }}</h5>
+            </div>
+
+            @endforeach
+          </div>
+
+        </div>
+        @endforeach
+    
+      </div>
+      <a class="carousel-control-prev" href="#carousel-popular" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carousel-popular" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  </div>
+
+
+</div>
+{{-- 
 <div class="row">
   <div class="container">
   <div id="carousel" class="carousel slide" data-ride="carousel">
@@ -47,8 +85,9 @@
     <div class="carousel-inner">
       @foreach($products as $key => $product)
       <div class="col-md-3">
-      <div class="carousel-item {{$key == 0 ? 'active' : '' }}">
-        <img class="d-block w-100"src="{{asset('images/thumbs/'.$product->image  ) }}">
+      <div class="carousel-item {{$key == 0 ? 'active' : '' }}"> 
+     {{--   <div class="carousel-item @if($loop->first) active @endif">--}}
+     {{--    <img class="d-block w-100"src="{{asset('images/thumbs/'.$product->image  ) }}">
         <div class="carousel-caption">
           <h5>test1</h5>
           <p>test1test1test1</p>
@@ -69,7 +108,7 @@
   </div>
 </div>
 </div>
-
+--}}
 
 
 
@@ -91,13 +130,13 @@
         @endforeach
         --}}
   </div> 
+  {{--
   <div class="col-lg-12">
    {{ $products->appends(request()->input())->links() }}
   </div>
+  --}}
 </div>
 @endsection
 
 
-<script>
-  $('.carousel').carousel()
-</script>
+
